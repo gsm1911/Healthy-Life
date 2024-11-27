8009665a82b4f47eab60c1867df10893296bd139
mathe <matheus.de.sene.pierri.carvalho@gmail.com>
2024-11-27 16:58:46 -0300

    id

--- app/src/main/java/com/example/healthy_life/dao/bean/LoginActivity.java
+++ app/src/main/java/com/example/healthy_life/dao/bean/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.healthy_life.dao.bean;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,6 +39,8 @@ public class LoginActivity extends AppCompatActivity {
          btnCriarConta.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
+                 Intent it = new Intent(LoginActivity.this, RegistroActivity.class);
+                 startActivity(it);
 
              }
          });
--- app/src/main/java/com/example/healthy_life/dao/bean/RegistroActivity.java
+++ app/src/main/java/com/example/healthy_life/dao/bean/RegistroActivity.java
@@ -1,4 +1,4 @@
-package com.example.healthy_life;
+package com.example.healthy_life.dao.bean;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +8,8 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.healthy_life.R;
 
 public class RegistroActivity extends AppCompatActivity{
     private EditText editNomeRegistro;
